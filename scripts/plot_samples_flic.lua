9156828bc02eedb02da40e3f7b98c01bb10190e1
farrajota <mglfarrajota@outlook.com>
2017-06-19 15:36:31 +0100

    Add scripts to plot heatmaps + skeletons for LSP + FLIC datasets

---
+++ scripts/plot_samples_flic.lua
@@ -0,0 +1,25 @@
+--[[
+    Plot body joint heatmaps and skeletons of random images from the FLIC dataset and save them do disk.
+]]
+
+
+configs = {
+    dataset = 'flic',
+    expID = 'final-best',
+    demo_nsamples = 5,
+    demo_plot_save = 'true',
+}
+
+--[[ concatenate options fields to a string ]]
+local str_args = ''
+for k, v in pairs(configs) do
+    str_args = str_args .. ('-%s %s '):format(k, v)
+end
+
+--[[ Setup command ]]
+command = 'qlua demo.lua ' .. str_args
+
+--[[ Run command ]]
+print('Executing command: ' .. command)
+print()
+os.execute(command)
