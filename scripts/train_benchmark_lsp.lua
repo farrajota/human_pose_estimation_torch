b31e2ec813bbcb8c9db410becc819f2292187cfc
farrajota <mglfarrajota@outlook.com>
2017-06-20 12:39:24 +0100

    Add scripts to process both train and benchmark steps

---
+++ scripts/train_benchmark_lsp.lua
@@ -0,0 +1,6 @@
+--[[
+    Train + benchmark the 'best' network arch on the LSP datasets.
+]]
+
+paths.dofile('train_lsp.lua')
+paths.dofile('benchmark_lsp.lua')
---
+++ scripts/train_benchmark_lsp_mpii.lua
@@ -0,0 +1,6 @@
+--[[
+    Train + benchmark the 'best' network arch on the LSP (+MPII) datasets.
+]]
+
+paths.dofile('train_mpii_lsp.lua')
+paths.dofile('benchmark_mpii_lsp.lua')
