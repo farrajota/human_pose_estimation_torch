4d61f14a800b56b504bc15d15aa26c2062e9a801
farrajota <mglfarrajota@outlook.com>
2017-06-23 15:09:04 +0100

    Refactored data generation test

--- tests/data_gen_batch.lua
+++ tests/data_gen_batch.lua
@@ -7,65 +7,29 @@ require 'paths'
 require 'string'
 disp = require 'display'
 
-torch.manualSeed(40)
+torch.manualSeed(4)
+torch.setdefaulttensortype('torch.FloatTensor')
 
 paths.dofile('../projectdir.lua') -- Project directory
 utils = paths.dofile('../util/utils.lua')
+paths.dofile('../data.lua')
 
 local opts = paths.dofile('../options.lua')
 opt = opts.parse(arg)
 opt.dataset = 'mpii+lsp'
 opt.rotRate=0.2
-
-paths.dofile('../dataset.lua')
-paths.dofile('../data.lua')
-
-dataset = loadDataset() -- load dataset train+val+test sets
-
---a,b = loadDataBenchmark(1,dataset.val, 'val')
+niters = 100
 mode = 'train'
---a,b = loadData(1,dataset[mode], mode)
-N = 16
-local img = torch.FloatTensor(N,3,256,256);
-local hm = {}
-for i=1, N do
-  print(i)
-  img[i], hm[i] = loadData(torch.random(dataset[mode].object:size(1)),dataset[mode], mode)
+
+local data_loader = select_dataset_loader(opt.dataset, mode)
+local loader = data_loader[mode]
+
+for i=1, niters do
+    print(('Iter %d/%d'):format(i, niters))
+    if i==24 then
+      aqui=1
+    end
+    local input, label = getSampleBatch(loader, opt.batchSize)
 end
 
-disp.image(img)
-
-function drawHeatmapParts(input, hms)
-
-    local function colorHM(x)
-        -- Converts a one-channel grayscale image to a color heatmap image
-        local function gauss(x,a,b,c)
-            return torch.exp(-torch.pow(torch.add(x,-b),2):div(2*c*c)):mul(a)
-        end
-        local cl = torch.zeros(3,x:size(1),x:size(2))
-        cl[1] = gauss(x,.5,.6,.2) + gauss(x,1,.8,.3)
-        cl[2] = gauss(x,1,.5,.3)
-        cl[3] = gauss(x,1,.2,.3)
-        cl[cl:gt(1)] = 1
-        return cl
-    end
-
-    local colorHms = {}
-    local inp64 = input:clone():mul(.3)
-    for i = 1,11 do 
-        colorHms[i] = colorHM(image.scale(hms[i],256))
-        colorHms[i]:mul(.7):add(inp64)
-    end
-    return colorHms
-end
-
-id = 5
---disp.image(drawHeatmapParts(img[5]:double(), hm[5]:double()))
-
-aqui=1
-
---print('Input size')
---print(#a)
---print('heatmap size')
---print(#b)
-
+print('Data fetching successfully finished.')
