87f8a7cbb23daf97d39d542325ce57e2a19a8457
farrajota <mglfarrajota@outlook.com>
2017-06-27 16:18:26 +0100

    Add train scripts for the coco dataset

--- tests/data_gen_batch.lua
+++ tests/data_gen_batch.lua
@@ -16,20 +16,32 @@ paths.dofile('../data.lua')
 
 local opts = paths.dofile('../options.lua')
 opt = opts.parse(arg)
-opt.dataset = 'lsp+mpii'
-opt.rotRate=0.2
+opt.dataset = 'coco'
+opt.rotRate = .2
 niters = 10000
 mode = 'train'
+plot_results = false
 
 local data_loader = select_dataset_loader(opt.dataset, mode)
 local loader = data_loader[mode]
 
 for i=1, niters do
     print(('Iter %d/%d'):format(i, niters))
-    if i==60 then
+    if i==6 then
         a=1  -- stop debugger here
     end
     local input, label = getSampleBatch(loader, opt.batchSize)
+
+    if plot_results then
+        a = {}
+        for i=1, input:size(1) do
+            local hm = label[i]
+            local im = input[i]
+            table.insert(a, drawImgHeatmapSingle(im, hm:sum(1):squeeze()))
+        end
+        disp.image(input)
+        disp.image(a)
+    end
 end
 
 print('Data fetching successfully finished.')
---
+++ tests/train_hg-stacked_coco.lua
@@ -0,0 +1,76 @@
+--[[
+    Train the 'hg-stacked' network arch on the COCO dataset.
+
+    Info:
+
+        Network name/id/type: generic (hourglass).
+
+        Train Ensemble: no.
+
+        Dataset: COCO.
+]]
+
+
+local function exec_command(command)
+    print('\n')
+    print('Executing command: ' .. command)
+    print('\n')
+    os.execute(command)
+end
+
+
+--------------------------------------------------------------------------------
+-- Train network
+--------------------------------------------------------------------------------
+
+do
+    local info = {
+        -- experiment id
+        expID = 'hg-stacked',
+        netType = 'hg-stacked',
+        dataset = 'coco',
+
+        -- data augment
+        colourNorm = 'false',
+        colourjit = 'true',
+        centerjit = 0,
+        pca = 'false',
+        dropout = 0,
+        spatialdropout = 0,
+        critweights = 'none',
+        scale = 0.30,
+        rotate = 40,
+        rotRate = 0.2,
+
+        -- train options
+        optMethod = 'adam',
+        nThreads = 2,
+        trainIters = 10000,
+        testIters = 4000,
+        nStack = 8,
+        nFeats = 256,
+        schedule = "{{75,2.5e-4,0},{15,1e-4,0},{10,5e-5,0}}",
+        batchSize = 4,
+        snapshot = 10,
+        nGPU = 1,
+        saveBest = 'true',
+        continue = 'false',
+        clear_buffers = 'true',
+    }
+
+    -- concatenate options fields to a string
+    local str_args = ''
+    for k, v in pairs(info) do
+        str_args = str_args .. ('-%s %s '):format(k, v)
+    end
+
+    local str_cuda
+    if info.nGPU <= 1 then
+        str_cuda = 'CUDA_VISIBLE_DEVICES=1'
+    else
+        str_cuda = 'CUDA_VISIBLE_DEVICES=1,0'
+    end
+
+    -- train network
+    exec_command(('%s th train.lua %s'):format(str_cuda, str_args))
+end
