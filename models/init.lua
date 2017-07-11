71b19fdc7e9da0b544c28fe91446197a43df7bbb
farrajota <mglfarrajota@outlook.com>
2017-07-11 15:51:16 +0100

    Update model list

--- models/init.lua
+++ models/init.lua
@@ -9,6 +9,7 @@ paths.dofile('test/init.lua')(model_list)
 model_list['hg-generic-best']     = paths.dofile('hg-generic-best.lua')  -- best combo of all models
 model_list['hg-generic-ensemble'] = paths.dofile('hg-generic-ensemble.lua')  -- Ensemble net
 model_list['hg-stacked']          = paths.dofile('hg-stacked.lua')
+model_list['hg-stacked-no-int']   = paths.dofile('hg-stacked-no-int.lua')
 model_list['hg-generic']          = paths.dofile('hg-generic.lua')
 model_list['sml']                 = paths.dofile('SML.lua')  -- SML net
 
